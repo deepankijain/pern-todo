993fd2f64846ca5dfc16d9677351f973f68c34a7
Deepanki Jain <deepankijain@gmail.com>
2021-09-13 09:14:46 +0530

    Added Client

---
+++ client/src/App.js
@@ -0,0 +1,16 @@
+import './App.css'
+import InputTodo from './components/InputTodo'
+import TodoList from './components/TodoList'
+
+function App() {
+  return (
+    <>
+      <div className='container'>
+        <InputTodo />
+        <TodoList />
+      </div>
+    </>
+  )
+}
+
+export default App
